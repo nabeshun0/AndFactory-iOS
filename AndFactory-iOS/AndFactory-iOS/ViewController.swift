5f515c4df9424b6a3be1d5ae9725b4eae49ced4f
snt-nbzw <sntnbzw@gmail.com>
2020-02-22 17:56:20 +0900

    プロジェクトファイルを追加

---
+++ AndFactory-iOS/AndFactory-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AndFactory-iOS
+//
+//  Created by member on 2020/02/22.
+//  Copyright © 2020 Shunta Nabezawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
