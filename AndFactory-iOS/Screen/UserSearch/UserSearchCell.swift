4f1c99d1a3bdaba48ced77272b7a37f863e2934a
snt-nbzw <sntnbzw@gmail.com>
2020-02-24 19:22:46 +0900

    UserSearchCellを追加 / 実装

---
+++ AndFactory-iOS/Screen/UserSearch/UserSearchCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class UserSearchCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userTypeLabel: UILabel!
+
+    public func configure() {
+        thumbnailImageView.image = #imageLiteral(resourceName: "snt-nbzw_icon")
+        userNameLabel.text = "snt-nbzw"
+        userTypeLabel.text = "User"
+    }
+
+}
