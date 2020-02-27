bb02049d8ce47cb9da15c3a395701f93563e1442
snt-nbzw <sntnbzw@gmail.com>
2020-02-27 11:43:49 +0900

    DataParser / AppRequestを実装

---
+++ AndFactory-iOS/Model/AppRequestType.swift
@@ -0,0 +1,38 @@
+import APIKit
+
+public class DecodableDataParser: DataParser {
+    public var contentType: String? {
+        return "application/json"
+    }
+
+    public func parse(data: Data) throws -> Any {
+        return data
+    }
+}
+
+public protocol AppRequestType: Request {}
+
+extension AppRequestType {
+
+    public var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+
+    public var headerFields: [String: String] {
+        return  ["Content-Type": "application/json",
+                 "charset": "utf-8"]
+    }
+}
+
+extension AppRequestType where Response: Decodable {
+    public var dataParser: DataParser {
+        return DecodableDataParser()
+    }
+
+    public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        guard let data = object as? Data else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        return try JSONDecoder().decode(Response.self, from: data)
+    }
+}
